e4025b9dc515fbd59d073a24076762dac730e8ea
AguzC <62857360+AguzC@users.noreply.github.com>
2025-09-11 21:58:14 -0300

    Update main.c

--- guiaAsm/src/4 - Memoria dinámica/main.c
+++ guiaAsm/src/4 - Memoria dinámica/main.c
@@ -7,7 +7,25 @@
 #include "../test-utils.h"
 #include "Memoria.h"
 
+uint64_t contarLetras(char* palabra) {
+	uint64_t res = 0;
+
+	while(*palabra != '\0') {
+		res += 1;
+		palabra += 1;
+
+	}
+
+	return res;
+} 
+
+
 int main() {
 	/* Acá pueden realizar sus propias pruebas */
+
+	char a[] = "hola";
+
+	printf("a tiene: %d letras \n", contarLetras(a));
+
 	return 0;
 }
